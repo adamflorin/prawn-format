7714229c28133471239e03cc66a443535596227d
Adam Florin <adam@adam-florins-macbook-2.local>
2010-07-28 12:42:42 -0700

    typo in underline geometry

--- lib/prawn/format/effects/underline.rb
+++ lib/prawn/format/effects/underline.rb
@@ -20,13 +20,13 @@ module Prawn
           # TODO: orient underline correctly for all rotation values (via trig).
           # For now, just support the 90-deg rotation we need with some pretty funky axis-bending.
           unless @options[:rotate] == 90
-            x1 = draw_state[:x] + @from_x
+            x1 = draw_state[:x] + @from
             x2 = draw_state[:x] + draw_state[:dx]
             y  = draw_state[:y] + draw_state[:dy] - 2.5 # larger-than-default offset
             document.move_to(x1, y)
             document.line_to(x2, y)
           else
-            y1 = draw_state[:y] + @from_x
+            y1 = draw_state[:y] + @from
             y2 = draw_state[:y] + draw_state[:dx]
             x = 7 # FIXME: get actual font size!
             document.move_to(x, y1)
